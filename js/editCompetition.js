291248704fbd2f2f2d25d7da1c4e454e690a8a0e
Jeremy Fleischman <jeremyfleischman@gmail.com>
2014-10-13 01:44:27 -0700

    now using less version of bootstrap, added typeahead/search as you type for usernames. Also added ian:accounts-ui-bootstrap-3, to make things a   little prettier.

--- js/editCompetition.js
+++ js/editCompetition.js
@@ -38,6 +38,58 @@ if(Meteor.isClient) {
     );
     return rounds;
   };
+
+  Template.editCompetition_users.usersAutocompleteSettings = function() {
+    // TODO - Client side workaround for
+    // https://github.com/mizzao/meteor-autocomplete/issues/43#issuecomment-58858328
+    var fakeCollection = {
+      find: function(selector, options) {
+        var allResults = Meteor.users.find(selector, options).fetch();
+        selector['profile.name'].$regex = "\\b" + selector['profile.name'].$regex;
+        var preferredResults = Meteor.users.find(selector, options).fetch();
+
+        var seenIds = {};
+        var arr = [];
+        var addResult = function(result) {
+          if(seenIds[result._id]) {
+            return;
+          }
+          seenIds[result._id] = true;
+          arr.push(result);
+        };
+        var i;
+        for(i = 0; i < preferredResults.length; i++) {
+          addResult(preferredResults[i]);
+        }
+        for(i = 0; i < allResults.length; i++) {
+          addResult(allResults[i]);
+        }
+        arr.count = function() { return arr.length; };
+        return arr;
+      }
+    };
+    return {
+      position: "top",
+      limit: 5,
+      rules: [
+        {
+          collection: fakeCollection,
+          field: "profile.name",
+          template: Template.userPill,
+          matchAll: true
+        }
+      ]
+    };
+  };
+
+  Template.editCompetition_users.users = function() {
+    // TODO - sort by name?
+    if(!this.userIds) {
+      return [];
+    }
+    return Meteor.users.find({_id: { $in: this.userIds } });
+  };
+
 }
 
 Competitions.allow({
