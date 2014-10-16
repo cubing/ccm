9801163c2f22e9bf17c75ffd1fdf85854ea5a521
Jeremy Fleischman <jeremyfleischman@gmail.com>
2014-10-16 03:58:11 -0700

    woah css is cool

--- client/util.js
+++
@@ -1,6 +0,0 @@
-Template.registerHelper("Titlecase", function(str) {
-  // Copied from http://stackoverflow.com/a/5574446
-  return str.replace(/\w\S*/g, function(txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
-});
