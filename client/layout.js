0a8012f567c792d36b550edd119c630066761713
Jeremy Fleischman <jeremyfleischman@gmail.com>
2014-11-03 23:58:58 -0800

    tweaked navbar a bit

---
+++ client/layout.js
@@ -0,0 +1,11 @@
+Template.layout.events({
+  "mouseover [data-toggle='tooltip']": function(e){
+    // Bootstrap's tooltips are opt in. Here we lazily enable it on all
+    // elements with a data-toggle="tooltip" attribute.
+    var $target = $(e.currentTarget);
+    if(!$target.data("tooltip-applied")){
+      $(e.currentTarget).tooltip('show');
+      $target.data("tooltip-applied", "true");
+    }
+  }
+});
