f464e43a3e9144796b4db8bbc3282ea305428312
Jeremy Fleischman <jeremyfleischman@gmail.com>
2014-11-05 11:01:11 -0800

    Cleaned up routes by using RouteController. Things unfortunately got a little hacky, but I think it's still an improvement.

--- common/routes.js
+++ common/routes.js
@@ -1,3 +1,9 @@
+Router.configure({
+  layoutTemplate: "layout",
+  loadingTemplate: "loading",
+  notFoundTemplate: "notFound",
+});
+
 if(Meteor.isClient){
   Router.onBeforeAction('dataNotFound');
   Template.registerHelper("title", function(){
@@ -20,173 +26,136 @@ var subscriptionError = function(that){
   };
 };
 
-Router.map(function(){
-  this.route('home', {
-    path: '/',
-    waitOn: function(){
-      return Meteor.subscribe('competitions', subscriptionError(this));
+ManageCompetitionController = RouteController.extend({
+  notFoundTemplate: "competitionNotFound",
+  waitOn: function(){
+    return [
+      Meteor.subscribe('competition', this.params.competitionUrlId, subscriptionError(this)),
+      Meteor.subscribe('competitionScrambles', this.params.competitionUrlId, subscriptionError(this))
+    ];
+  },
+  data: function(){
+    var competitionUrlId = this.params.competitionUrlId;
+    var competition = Competitions.findOne({
+      $or: [
+        { _id: competitionUrlId },
+        { wcaCompetitionId: competitionUrlId }
+      ]
+    });
+    if(!competition) {
+      return null;
     }
-  });
-
-  this.route('newCompetition', {
-    path: '/new'
-  });
-
-  // TODO - use iron-router's route controllers
-  var editCompetitionRouteInfo = {
-    notFoundTemplate: "competitionNotFound",
-    waitOn: function(){
-      return [
-        Meteor.subscribe('competition', this.params.competitionUrlId, subscriptionError(this)),
-        Meteor.subscribe('competitionScrambles', this.params.competitionUrlId, subscriptionError(this))
-      ];
-    },
-    data: function(){
-      var competitionUrlId = this.params.competitionUrlId;
-      var competition = Competitions.findOne({
-        $or: [
-          { _id: competitionUrlId },
-          { wcaCompetitionId: competitionUrlId }
-        ]
-      });
-      if(!competition) {
-        return null;
-      }
-      return {
-        competitionUrlId: competitionUrlId,
-        competition: competition
-      };
-    }
-  };
-  this.route('manageCompetition', _.extend({
-    template: 'editCompetition',
-    path: "/:competitionUrlId/manage"
-  }, editCompetitionRouteInfo));
-  this.route('uploadScrambles', _.extend({
-    path: "/:competitionUrlId/manage/uploadScrambles"
-  }, editCompetitionRouteInfo));
-  this.route('exportResults', _.extend({
-    path: "/:competitionUrlId/manage/exportResults"
-  }, editCompetitionRouteInfo));
-
-  this.route('competition', {
-    path: "/:competitionUrlId",
-    waitOn: function(){
-      return Meteor.subscribe('competition', this.params.competitionUrlId, subscriptionError(this));
-    },
-    data: function(){
-      var competitionUrlId = this.params.competitionUrlId;
-      var competition = Competitions.findOne({
-        $or: [
-          { _id: competitionUrlId },
-          { wcaCompetitionId: competitionUrlId }
-        ]
-      }, {
-        fields: {
-          wcaCompetitionId: 1,
-          competitionName: 1,
-          organizers: 1,
-          staff: 1
-        }
-      });
-      if(!competition) {
-        return null;
-      }
-      return {
-        competition: competition,
-        competitionUrlId: competitionUrlId
-      };
-    }
-  });
-  this.route('round', {
-    path: "/:competitionUrlId/:eventCode/:roundCode",
-    template: 'round',
-    waitOn: function(){
-      return Meteor.subscribe('competition', this.params.competitionUrlId, subscriptionError(this));
-    },
-    data: function(){
-      var competitionUrlId = this.params.competitionUrlId;
-      var roundCode = this.params.roundCode;
-      var eventCode = this.params.eventCode;
-
-      var competition = Competitions.findOne({
-        $or: [
-          { _id: competitionUrlId },
-          { wcaCompetitionId: competitionUrlId }
-        ]
-      }, {
-        fields: {
-          wcaCompetitionId: 1,
-          competitionName: 1,
-          organizers: 1,
-          staff: 1
-        }
-      });
-      if(!competition){
-        return null;
-      }
-
-      var round = Rounds.findOne({
-        competitionId: competition._id,
-        eventCode: eventCode,
-        roundCode: roundCode
-      });
-      return {
-        competitionUrlId: competitionUrlId,
-        competition: competition,
-        round: round
-      };
-    }
-  });
-  this.route('competitor', {
-    path: "/:competitionUrlId/:competitorName",
-    template: 'competitor',
-    waitOn: function(){
-      return [
-        Meteor.subscribe('competition', this.params.competitionUrlId, subscriptionError(this))
-      ];
-    },
-    data: function(){
-      var competitionUrlId = this.params.competitionUrlId;
-      var userName = this.params.competitorName;
-
-
-      var competition = Competitions.findOne({
-        $or: [
-          { _id: competitionUrlId },
-          { wcaCompetitionId: competitionUrlId }
-        ]
-      }, {
-        fields: {
-          wcaCompetitionId: 1,
-          competitionName: 1,
-          organizers: 1,
-          staff: 1
-        }
-      });
-      if(!competition){
-        return null;
-      }
-
-      var user = Meteor.users.findOne({
-        "profile.name": userName
-      });
-
-      if(!user){
-        return null;
-      }
-
-      return {
-        competitionUrlId: competitionUrlId,
-        competition: competition,
-        user: user
-      };
-    }
-  });
+    return {
+      competitionUrlId: competitionUrlId,
+      competition: competition
+    };
+  }
 });
 
-Router.configure({
-  layoutTemplate: "layout",
-  loadingTemplate: "loading",
-  notFoundTemplate: "notFound",
+ViewCompetitionController = RouteController.extend({
+  notFoundTemplate: "competitionNotFound",
+  waitOn: function(){
+    return Meteor.subscribe('competition', this.params.competitionUrlId, subscriptionError(this));
+  },
+  data: function(){
+    var competitionUrlId = this.params.competitionUrlId;
+    var competition = Competitions.findOne({
+      $or: [
+        { _id: competitionUrlId },
+        { wcaCompetitionId: competitionUrlId }
+      ]
+    }, {
+      fields: {
+        wcaCompetitionId: 1,
+        competitionName: 1,
+        organizers: 1,
+        staff: 1
+      }
+    });
+    if(!competition) {
+      return null;
+    }
+    return {
+      competition: competition,
+      competitionUrlId: competitionUrlId
+    };
+  }
+});
+
+Router.route('/', {
+  name: 'home',
+  waitOn: function(){
+    return Meteor.subscribe('competitions', subscriptionError(this));
+  }
+});
+
+Router.route('/new', {
+  name: 'newCompetition'
+});
+
+Router.route('/:competitionUrlId/manage', {
+  name: 'manageCompetition',
+  template: 'editCompetition',
+  controller: 'ManageCompetitionController'
+});
+Router.route('/:competitionUrlId/manage/uploadScrambles', {
+  name: 'uploadScrambles',
+  controller: 'ManageCompetitionController',
+});
+Router.route('/:competitionUrlId/manage/exportResults', {
+  name: 'exportResults',
+  controller: 'ManageCompetitionController',
+});
+
+Router.route('/:competitionUrlId', {
+  name: 'competition',
+  controller: 'ViewCompetitionController'
+});
+
+Router.route('/:competitionUrlId/:eventCode/:roundCode', {
+  name: 'round',
+  controller: 'ViewCompetitionController',
+  data: function(){
+    var roundCode = this.params.roundCode;
+    var eventCode = this.params.eventCode;
+
+    // Hack to call our controller's data function
+    var data = this.constructor.prototype.data.call(this);
+    if(!data){
+      return null;
+    }
+
+    var round = Rounds.findOne({
+      competitionId: data.competition._id,
+      eventCode: eventCode,
+      roundCode: roundCode
+    });
+    if(!round) {
+      return null;
+    }
+    data.round = round;
+    return data;
+  }
+});
+Router.route('/:competitionUrlId/:competitorName', {
+  name: 'competitor',
+  controller: 'ViewCompetitionController',
+  data: function(){
+    var userName = this.params.competitorName;
+
+    // Hack to call our controller's data function
+    var data = this.constructor.prototype.data.call(this);
+    if(!data){
+      return null;
+    }
+
+    var user = Meteor.users.findOne({
+      "profile.name": userName
+    });
+    if(!user) {
+      return null;
+    }
+    data.user = user;
+    return data;
+  }
 });
