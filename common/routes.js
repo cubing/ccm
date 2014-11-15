1739477be5d294346c4075200b83bad7de58e7a1
Jeremy Fleischman <jeremyfleischman@gmail.com>
2014-11-15 02:23:41 -0800

    We're now setting html titles! This fixes #31.

--- common/routes.js
+++ common/routes.js
@@ -4,22 +4,56 @@ Router.configure({
   notFoundTemplate: "notFound",
 });
 
-if(Meteor.isClient){
+if(Meteor.isClient) {
   Router.onBeforeAction('dataNotFound');
-  Template.registerHelper("title", function(){
-    return Router.current().route.getName();
+
+  Template.registerHelper("isActiveRoute", function(routeName) {
+    return Router.current().route.getName() == routeName;
   });
 
-  Template.registerHelper("isActiveRoute", function(routeName){
-    return Router.current().route.getName() == routeName;
+  Template.registerHelper("refreshTitle", function() {
+    var title = "live.cubing.net";
+
+    var data = Router.current().data && Router.current().data();
+    if(data) {
+      if(data.competitionId) {
+        var competitionName = getCompetitionAttribute(data.competitionId, 'competitionName');
+        title = competitionName + " - " + title;
+      }
+      if(data.roundId) {
+        var eventCode = getRoundAttribute(data.roundId, 'eventCode');
+        var eventName = wca.eventByCode[eventCode].name;
+
+        var roundCode = getRoundAttribute(data.roundId, 'roundCode');
+        var roundName = wca.roundByCode[roundCode].name;
+
+        title = wca.eventByCode[eventCode].name + " " + roundName + " - " + title;
+      }
+      if(data.user) {
+        title = data.user.profile.name + " - " + title;
+      }
+    }
+
+    var titlePrefix = Router.current().lookupOption('titlePrefix');
+    if(titlePrefix) {
+      title = titlePrefix + " - " + title;
+    }
+
+    // Immediately updating the page title here was causing the title of the
+    // current (new) page to get put in the history as the title of the
+    // previous (old) page. Waiting for a little bit before setting the title
+    // lets the browser history look sane.
+    setTimeout(function() {
+      document.title = title;
+    }, 0);
   });
 }
 
 // It appears that iron-router does nothing useful when a subscription throws
 // an error. We explicitly catch that error, log it, and then render 'notFound'
-var subscriptionError = function(that){
+var subscriptionError = function(that) {
   return {
-    onError: function(err){
+    onError: function(err) {
       console.error(err);
       that.render('notFound');
     }
@@ -28,14 +62,14 @@ var subscriptionError = function(that){
 
 ManageCompetitionController = RouteController.extend({
   notFoundTemplate: "competitionNotFound",
-  waitOn: function(){
+  waitOn: function() {
     return [
       Meteor.subscribe('competition', this.params.competitionUrlId, subscriptionError(this)),
       // TODO - we only need scrambles on the uploadScrambles route
       Meteor.subscribe('competitionScrambles', this.params.competitionUrlId, subscriptionError(this))
     ];
   },
-  data: function(){
+  data: function() {
     var competitionUrlId = this.params.competitionUrlId;
     var competition = Competitions.findOne({
       $or: [
@@ -54,15 +88,15 @@ ManageCompetitionController = RouteController.extend({
       competitionUrlId: competitionUrlId,
       competitionId: competition._id
     };
-  }
+  },
 });
 
 ViewCompetitionController = RouteController.extend({
-  waitOn: function(){
+  waitOn: function() {
     return Meteor.subscribe('competition', this.params.competitionUrlId, subscriptionError(this));
   },
-  data: function(){
-    if(!this.ready()){
+  data: function() {
+    if(!this.ready()) {
       // We explicitly render *NotFound templates based on what's missing, and
       // that steps on the toes of iron-router's loading hook. If we're not
       // ready, just do nothing and let the loading hook render our
@@ -93,52 +127,60 @@ ViewCompetitionController = RouteController.extend({
 
 Router.route('/', {
   name: 'home',
-  waitOn: function(){
+  waitOn: function() {
     return Meteor.subscribe('competitions', subscriptionError(this));
-  }
+  },
+  titlePrefix: '',
 });
 
 Router.route('/settings/profile', {
-  name: 'editProfile'
+  name: 'editProfile',
+  titlePrefix: 'Edit profile',
 });
 
 Router.route('/new', {
-  name: 'newCompetition'
+  name: 'newCompetition',
+  titlePrefix: 'New competition',
 });
 
 Router.route('/:competitionUrlId/manage', {
   name: 'manageCompetition',
   template: 'editCompetition',
-  controller: 'ManageCompetitionController'
+  controller: 'ManageCompetitionController',
+  titlePrefix: "Manage",
 });
 Router.route('/:competitionUrlId/manage/uploadScrambles', {
   name: 'uploadScrambles',
   controller: 'ManageCompetitionController',
+  titlePrefix: "Upload scrambles",
 });
 Router.route('/:competitionUrlId/manage/exportResults', {
   name: 'exportResults',
   controller: 'ManageCompetitionController',
+  titlePrefix: "Export results",
 });
 Router.route('/:competitionUrlId/manage/schedule', {
   name: 'editSchedule',
   controller: 'ManageCompetitionController',
+  titlePrefix: "Edit schedule",
 });
 
 Router.route('/:competitionUrlId', {
   name: 'competition',
-  controller: 'ViewCompetitionController'
+  controller: 'ViewCompetitionController',
+  titlePrefix: null,
 });
 
 Router.route('/:competitionUrlId/:eventCode/:roundCode', {
   name: 'round',
   controller: 'ViewCompetitionController',
-  data: function(){
+  data: function() {
     var roundCode = this.params.roundCode;
     var eventCode = this.params.eventCode;
 
     // Hack to call our controller's data function
     var data = this.constructor.prototype.data.call(this);
-    if(!data){
+    if(!data) {
       return null;
     }
 
@@ -157,17 +199,17 @@ Router.route('/:competitionUrlId/:eventCode/:roundCode', {
     }
     data.roundId = round._id;
     return data;
-  }
+  },
 });
 Router.route('/:competitionUrlId/:competitorName', {
   name: 'competitor',
   controller: 'ViewCompetitionController',
-  data: function(){
+  data: function() {
     var userName = this.params.competitorName;
 
     // Hack to call our controller's data function
     var data = this.constructor.prototype.data.call(this);
-    if(!data){
+    if(!data) {
       return null;
     }
 
