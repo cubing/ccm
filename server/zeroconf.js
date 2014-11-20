f1a18a61bda608a975a8808197be7cc02c5df4c1
good job <gjcomps@ip-172-31-21-4.us-west-1.compute.internal>
2014-11-20 04:27:51 +0000

    Fallback to port 80 if there is no port specified.

--- server/zeroconf.js
+++ server/zeroconf.js
@@ -7,7 +7,7 @@ Meteor.startup(function() {
   // Wow, this is so gross. I couldn't find any way to get to
   // our "runner" though.
   // See https://github.com/meteor/meteor/blob/devel/tools/run-all.js#L344
-  var port = url.parse(process.env.ROOT_URL).port;
+  var port = url.parse(process.env.ROOT_URL).port || 80;
   var service = new mdns.createAdvertisement(
     mdns.tcp('_http'),
     "" + port,
