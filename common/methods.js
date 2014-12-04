cubing/ccm

commit fdda5444a449e9025bc985bdf69fa6df654e8b19

nthRound changed to be 1 indexed, and round routes changed to use nthRound instead of event code. This means that urls won't become invalid or change radically if organizers add rounds or introduce cutoffs. Added ongoing round selectors to /manage/data-entry/.

common/methods.js
@@ -103,10 +103,10 @@ Meteor.methods({
       // rounds (not that that's something we expect to ever happen, since
       // removeRound only allows removal of the latest round).
       var supportedRoundsIndex;
-      if(nthRound == rounds.length - 1) {
+      if(nthRound == rounds.length) {
         supportedRoundsIndex = wca.MAX_ROUNDS_PER_EVENT - 1;
       } else {
-        supportedRoundsIndex = nthRound;
+        supportedRoundsIndex = nthRound - 1;
       }
       var roundCodes = wca.supportedRounds[supportedRoundsIndex];
       var roundCode = round.softCutoff ? roundCodes.combined : roundCodes.uncombined;

client/competitionEvents.js
@@ -10,7 +10,7 @@ Template.competitionEvents.helpers({
         nthRound: 1,
       }
     });
-    return _.range(1, mostAdvancedRound.nthRound + 2);
+    return _.range(0, mostAdvancedRound.nthRound + 1);
   },
   events: function() {
     return getCompetitionEvents(this.competitionId);

client/dataEntry.js
@@ -1,0 +1,14 @@
+Template.dataEntry.helpers({
+  openRounds: function() {
+    var openRounds = Rounds.find({
+      competitionId: this.competitionId,
+      status: wca.roundStatuses.open,
+    });
+    return openRounds;
+  },
+  isSelectedRound: function() {
+    var data = Template.parentData(1);
+    var selectedRoundId = data.roundId;
+    return selectedRoundId == this._id;
+  },
+});

client/editCompetition.js
@@ -468,7 +468,7 @@ Template.editCompetition.helpers({
       eventCode: this.eventCode
     }, {
       sort: {
-        "nthRound": 1
+        nthRound: 1
       }
     }).fetch();
     return rounds;

client/layout.js
@@ -34,6 +34,13 @@ Template.layout.events({
   },
 });
 
+Router.onBeforeAction(function() {
+  // As the page changes, remove any leftover tooltips that might get
+  // abandoned because they were set to data-container="body"
+  $('.tooltip').remove();
+  this.next();
+});
+
 var verificationSendSuccessReact = new ReactiveVar(null);
 
 Template.layout.helpers({

common/_collections.js
@@ -140,9 +140,9 @@ Rounds.attachSchema({
 
   // *** Attributes for real rounds for WCA events ***
   nthRound: {
-    // How many rounds come before this round for this eventCode
+    // Indexed from 1, because humans will see this in urls
     type: Number,
-    min: 0,
+    min: 1,
     optional: true,
   },
   size: {

common/helpers.js
@@ -1,9 +1,4 @@
 if(Meteor.isClient) {
-  // oh handlebars
-  Template.registerHelper("add", function(a, b) {
-    return a + b;
-  });
-
   Template.registerHelper("roundName", function(roundCode) {
     return wca.roundByCode[roundCode].name;
   });

common/routes.js
@@ -139,6 +139,41 @@ ViewCompetitionController = RouteController.extend({
   }
 });
 
+function getRoundData() {
+  // Hack to call our controller's data function
+  var data = this.constructor.prototype.data.call(this);
+  if(!data) {
+    return null;
+  }
+
+  if(!this.params.nthRound) {
+    data.eventCode = this.params.eventCode;
+    return data;
+  }
+  if(!String.isNonNegInt(this.params.nthRound)) {
+    this.render('roundNotFound');
+    return;
+  }
+  var nthRound = parseInt(this.params.nthRound);
+  var eventCode = this.params.eventCode;
+
+  var round = Rounds.findOne({
+    competitionId: data.competitionId,
+    eventCode: eventCode,
+    nthRound: nthRound,
+  }, {
+    fields: {
+      _id: 1,
+    }
+  });
+  if(!round) {
+    this.render('roundNotFound');
+    return;
+  }
+  data.roundId = round._id;
+  return data;
+}
+
 Router.route('/', {
   name: 'home',
   waitOn: function() {
@@ -183,10 +218,13 @@ Router.route('/:competitionUrlId/manage/schedule', {
   controller: 'ManageCompetitionController',
   titlePrefix: "Edit schedule",
 });
-Router.route('/:competitionUrlId/manage/data-entry', {
+Router.route('/:competitionUrlId/manage/data-entry/:eventCode?/:nthRound?', {
   name: 'dataEntry',
   controller: 'ManageCompetitionController',
   titlePrefix: "Data entry",
+  data: function() {
+    return getRoundData.call(this);
+  },
 });
 
 Router.route('/:competitionUrlId', {
@@ -214,34 +252,11 @@ Router.route('/:competitionUrlId/results', {
   controller: 'ViewCompetitionController',
   titlePrefix: 'Results',
 });
-Router.route('/:competitionUrlId/results/:eventCode/:roundCode', {
+Router.route('/:competitionUrlId/results/:eventCode/:nthRound', {
   name: 'roundResults',
   controller: 'ViewCompetitionController',
   data: function() {
-    var roundCode = this.params.roundCode;
-    var eventCode = this.params.eventCode;
-
-    // Hack to call our controller's data function
-    var data = this.constructor.prototype.data.call(this);
-    if(!data) {
-      return null;
-    }
-
-    var round = Rounds.findOne({
-      competitionId: data.competitionId,
-      eventCode: eventCode,
-      roundCode: roundCode,
-    }, {
-      fields: {
-        _id: 1,
-      }
-    });
-    if(!round) {
-      this.render('roundNotFound');
-      return;
-    }
-    data.roundId = round._id;
-    return data;
+    return getRoundData.call(this);
   },
 });
 Router.route('/:competitionUrlId/results/:competitorName', {

commit 3b26db18c56619f3728763092f6d6810b5266b8c

Fix nthRound calculation in refreshRoundCodes (fallout from fdda5444a449e9025bc985bdf69fa6df654e8b19)

common/methods.js
@@ -97,25 +97,26 @@ Meteor.methods({
     if(rounds.length > wca.MAX_ROUNDS_PER_EVENT) {
       throw new Meteor.Error(400, "Too many rounds");
     }
-    rounds.forEach(function(round, nthRound) {
+    rounds.forEach(function(round, index) {
       // Note that we ignore the actual value of nthRound, and instead use the
       // index into rounds as the nthRound. This defragments any missing
       // rounds (not that that's something we expect to ever happen, since
       // removeRound only allows removal of the latest round).
       var supportedRoundsIndex;
-      if(nthRound == rounds.length) {
+      if(index == rounds.length) {
         supportedRoundsIndex = wca.MAX_ROUNDS_PER_EVENT - 1;
       } else {
-        supportedRoundsIndex = nthRound - 1;
+        supportedRoundsIndex = index;
       }
       var roundCodes = wca.supportedRounds[supportedRoundsIndex];
+      assert(roundCodes);
       var roundCode = round.softCutoff ? roundCodes.combined : roundCodes.uncombined;
       Rounds.update({
-        _id: round._id
+        _id: round._id,
       }, {
         $set: {
           roundCode: roundCode,
-          nthRound: nthRound
+          nthRound: index + 1,
         }
       });
     });