461062b96ad624266303ab4015ac6bc4099be6b9
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-01-25 00:04:30 -0800

    I'm gonna mark this as fixes #148.

--- both/new_assert.js
+++ both/new_assert.js
@@ -1,9 +1,11 @@
+// Workaround for https://github.com/jfly/ccm/issues/148
+
 assert = function(condition) {
   if(!condition) {
-    throw new Meteor.Error(500);//<<<
+    throw new Meteor.Error(500);
   }
 };
 
 assert.equal = function(expected, actual) {
-  assert(expected == actual);//<<<
+  assert(expected == actual);
 };
