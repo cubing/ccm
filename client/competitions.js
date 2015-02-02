d0fb5729a0811bb27ebcc8352f1718cd52d25df0
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-02-02 00:15:39 -0800

    Cleanup homepage so things don't look completely ridiculous.

--- client/competitions.js
+++ client/competitions.js
@@ -1,37 +1,11 @@
 Template.competitions.helpers({
-  allComps: function() {
+  competitions: function() {
     return Competitions.find({}, {
       fields: {
         wcaCompetitionId: 1,
         competitionName: 1,
-        listed: 1
+        listed: 1,
       }
     });
   },
-
-  listedClass: function() {
-    return this.listed ? "listed" : "unlisted";
-  },
-
-  myCompetitions: function() {
-    var myCompetitions = Competitions.find(
-      { organizers: { $elemMatch: { $in: [ Meteor.userId() ] } } }
-    );
-    return myCompetitions;
-  },
-});
-
-Template.competitions.events({
-  'submit #newCompetitionForm': function(event) {
-    event.preventDefault();
-    var form = event.currentTarget;
-    var competitionName = form.competitionName.value;
-    form.competitionName.value = '';
-    form.competitionName.blur();
-    Meteor.call('createCompetition', competitionName);
-  },
-  'click .deleteCompetition': function(event) {
-    var competitionId = this._id;
-    Meteor.call('deleteCompetition', competitionId);
-  }
 });
