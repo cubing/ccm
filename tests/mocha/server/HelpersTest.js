11eebcee85e98271172c545df6d990fb9dbe3310
Lars <github@lar5.com>
2015-02-23 12:43:14 -0800

    Fixed several problems with first commit. Added test for event ordering.

--- tests/mocha/server/HelpersTest.js
+++ tests/mocha/server/HelpersTest.js
@@ -2,21 +2,24 @@ MochaWeb.testOnly(function(){
   describe('helpers', function() {
 
     describe('getCompetitionEvents', function() {
-      it('gets events', function() {
-        chai.expect(getCompetitionEvents(null)).to.deep.equal([]);
+      var compId = Competitions.insert({ competitionName: "Comp Etition", listed: false, startDate: new Date() });
 
-        var compId = Competitions.insert({ competitionName: "Comp One", listed: false, startDate: new Date() });
+      it('returns only competition events, in expected format', function() {
+        chai.expect(getCompetitionEvents(null)).to.deep.equal([]);
         chai.expect(getCompetitionEvents(compId)).to.deep.equal([]);
 
-        Rounds.insert({ competitionId: compId, eventCode: '333', formatCode: '2', roundCode: 'f', nthRound: 1 });
-        Rounds.insert({ competitionId: compId, title: 'non competition' });
+        Rounds.insert({ competitionId: compId, eventCode: '333' });
+        Rounds.insert({ competitionId: compId, title: 'not a competition' });
+        Rounds.insert({ competitionId: 'other competition', eventCode: '666' });
 
         chai.expect(getCompetitionEvents(compId)).to.deep.equal([{competitionId: compId, eventCode: '333'}]);
+      });
 
-        Rounds.insert({ competitionId: compId, eventCode: '222', formatCode: '2', roundCode: 'f', nthRound: 1 });
-
-        // This assumes Mongo returns things in this order. Order independent deep comparison in Chai is strangely hard.
-        chai.expect(getCompetitionEvents(compId)).to.deep.equal([{competitionId: compId, eventCode: '222'}, {competitionId: compId, eventCode: '333'}]);
+      it('returns events in WCA order', function() {
+        ['333', '222', '333mbf', '777', 'clock'].forEach(function(code) {
+          Rounds.insert({ competitionId: compId, eventCode: code });
+        });
+        chai.expect(_.pluck(getCompetitionEvents(compId), 'eventCode').join()).to.equal('222,333,777,clock,333mbf');
       });
     });
 
