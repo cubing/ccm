f41c09425c47a7e26eca852c2fb9b3b9e84fdff8
limabeans <toomanytypos@gmail.com>
2015-03-12 20:20:16 -0700

    updated competitorResults.html, although not sure how to parse RoundIds

--- client/competitorResults.js
+++ client/competitorResults.js
@@ -1,9 +1,28 @@
 Template.participantResults.helpers({
-  results: function() {
-    var results = Results.find({
-      competitionId: this.competitionId,
-      registrationId: this.registration._id,
-    });
-    return results;
-  }
+    results: function() {
+	var results = Results.find({
+	    competitionId: this.competitionId,
+	    registrationId: this.registration._id,
+	});
+	return results;
+    },
+    roundId: function() {
+	return this.roundId;
+    },
+    average: function() {
+	return this.average;
+    },
+    best: function() {
+	return this.solves[this.bestIndex];
+    },
+    competitorName: function() {
+	return this.registration.uniqueName;
+    },
+    numSolves: function() {
+	return 5;
+    },
+    wcaProfileUrl: function() {
+	var wcaId = this.registration.wcaId;
+	return 'https://www.worldcubeassociation.org/results/p.php?i='+wcaId;
+    }
 });
---
+++ client/participantNotFound.js
@@ -0,0 +1,6 @@
+Template.participantNotFound.helpers({
+    competitionUrlId: function() {
+	console.log(this);
+	return this.competitionUrlId;
+    }
+});
