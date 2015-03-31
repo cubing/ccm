36e34be549b5951bd49e3b2dc37362753563a134
Lars <github@lar5.com>
2015-03-31 10:51:32 -0700

    Made methods test work with manual stub

--- tests/mocha/server/MethodsTest.js
+++ tests/mocha/server/MethodsTest.js
@@ -1,37 +1,35 @@
 MochaWeb.testOnly(function() {
   describe('Methods', function() {
 
-    it('deleteCompetition (DOES NOT WORK)', function() {
-      // Need to figure out how to either Log in a User, or mock out the user check.
+    it('deleteCompetition', function() {
+      var preComps = Competitions.find().count();
+      var preRounds = Rounds.find().count();
+      var preProgs = RoundProgresses.find().count();
 
-//      var preComps = Competitions.find().count();
-//      var preRounds = Rounds.find().count();
-//      var preProgs = RoundProgresses.find().count();
-//
-//      var comp1Id = Competitions.insert({ competitionName: "Comp One", listed: false, startDate: new Date() });
-//      var comp2Id = Competitions.insert({ competitionName: "Comp Two", listed: false, startDate: new Date() });
-//
-//      [comp1Id, comp2Id].forEach(function(compId) {
-//        var roundId = Rounds.insert({ competitionId: compId, eventCode: '333' });
-//        RoundProgresses.insert({ competitionId: compId, roundId: roundId });
-//      });
-//
-//      chai.expect(Competitions.find().count() - preComps).to.equal(2);
-//      chai.expect(Rounds.find().count() - preRounds).to.equal(2);
-//      chai.expect(RoundProgresses.find().count() - preProgs).to.equal(2);
-//
-//
-//      var old = throwIfCannotManageCompetition;
-//      throwIfCannotManageCompetition = function(userId, competitionId) {};
-//
-//      Meteor.call('deleteCompetition', comp1Id);
-//
-//      throwIfCannotManageCompetition = old;
-//
-//      chai.expect(Competitions.find().count() - preComps).to.equal(1);
-//      chai.expect(Rounds.find().count() - preRounds).to.equal(1);
-//      chai.expect(RoundProgresses.find().count() - preProgs).to.equal(1);
+      var comp1Id = Competitions.insert({ competitionName: "Comp One", listed: false, startDate: new Date() });
+      var comp2Id = Competitions.insert({ competitionName: "Comp Two", listed: false, startDate: new Date() });
 
+      [comp1Id, comp2Id].forEach(function(compId) {
+        var roundId = Rounds.insert({ competitionId: compId, eventCode: '333' });
+        RoundProgresses.insert({ competitionId: compId, roundId: roundId });
+      });
+
+      chai.expect(Competitions.find().count() - preComps).to.equal(2);
+      chai.expect(Rounds.find().count() - preRounds).to.equal(2);
+      chai.expect(RoundProgresses.find().count() - preProgs).to.equal(2);
+
+      var g = global;
+
+      var old = g.throwIfCannotManageCompetition;
+      g.throwIfCannotManageCompetition = function(userId, competitionId) {};
+
+      Meteor.call('deleteCompetition', comp1Id);
+
+      g.throwIfCannotManageCompetition = old;
+
+      chai.expect(Competitions.find().count() - preComps).to.equal(1);
+      chai.expect(Rounds.find().count() - preRounds).to.equal(1);
+      chai.expect(RoundProgresses.find().count() - preProgs).to.equal(1);
     });
   });
 });
