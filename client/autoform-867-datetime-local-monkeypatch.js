f41342de98dc3ce7a83a0c98c2ed1f9f4dee8836
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-04-07 16:08:09 -0700

    Monkeypatch workaround for https://github.com/aldeed/meteor-autoform/issues/867. Removed another autoform workaround that has since been fixed =).

---
+++ client/autoform-867-datetime-local-monkeypatch.js
@@ -0,0 +1,14 @@
+// Dirty workaround for https://github.com/aldeed/meteor-autoform/issues/867
+AutoForm.Utility.dateToNormalizedLocalDateAndTimeString = function dateToNormalizedLocalDateAndTimeString(date, timezoneId) {
+  var m = moment(date);
+  // by default, we assume local timezone; add moment-timezone to app and pass timezoneId
+  // to use a different timezone
+  if (typeof timezoneId === "string") {
+    if (typeof m.tz !== "function") {
+      throw new Error("If you specify a timezoneId, make sure that you've added a moment-timezone package to your app");
+    }
+    m.tz(timezoneId);
+  }
+  return m.format("YYYY-MM-DD[T]HH:mm");//CCM
+  //CCM return m.format("YYYY-MM-DD[T]HH:mm:ss.SSS");
+};
