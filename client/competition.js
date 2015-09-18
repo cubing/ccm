3503bc745d527ce1a768f90953a7cf0af3b2b3b6
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-09-18 08:29:38 -0700

    Fixed bug with registeredForCompetition. This fixes #252.

--- client/competition.js
+++ client/competition.js
@@ -1,14 +1,12 @@
 Template.competition.helpers({
   registeredForCompetition: function() {
-    var registrations = Registrations.find({
+    var registration = Registrations.findOne({
       competitionId: this.competitionId,
       userId: Meteor.userId(),
     }, {
       fields: { _id: 1 }
-    }).fetch();
-
-    var registered = _.contains(_.pluck(registrations, '_id'), Meteor.userId()); // TODO Does this even work?
-    return registered;
+    });
+    return !!registration;
   },
   competitionIsScheduled: function() {
     return Competitions.findOne(this.competitionId).startDate;
