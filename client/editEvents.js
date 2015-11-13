cubing/ccm

commit 9c067e58ed8a9279b17a8efc7b45865591d93950

A few safe cleanups:
- Replace Mongo "op({ _id: X}," with "op(X,"
- Replace UI.contentBlock with more modern Template.contentBlock
- Replace setTimeout(..., 0) with Meteor.defer(...)

Also tried to replace Template .rendered, .created, .destroyed with the undeprecated .onRendered etc, but things stopped working.

client/editEvents.js
@@ -18,16 +18,16 @@ Template.editEvents.events({
     Meteor.call('removeLastRound', this.competitionId, this.eventCode);
   },
   'click button[name="buttonOpenRound"]': function(e, template) {
-    Rounds.update({ _id: this._id }, { $set: { status: wca.roundStatuses.open } });
+    Rounds.update(this._id, { $set: { status: wca.roundStatuses.open } });
   },
   'click button[name="buttonCloseRound"]': function(e, template) {
-    Rounds.update({ _id: this._id }, { $set: { status: wca.roundStatuses.closed } });
+    Rounds.update(this._id, { $set: { status: wca.roundStatuses.closed } });
   },
   'change select[name="roundFormat"]': function(e) {
     var select = e.currentTarget;
     var formatCode = select.value;
     var roundId = this._id;
-    Rounds.update({ _id: roundId }, { $set: { formatCode: formatCode } });
+    Rounds.update(roundId, { $set: { formatCode: formatCode } });
   },
   'click button[name="buttonSetRoundSize"]': function(e, template) {
     roundPopupReact.set({ setRoundSize: this });
@@ -281,7 +281,7 @@ Template.modalSetRoundSize.events({
     } else {
       toSet.$unset = { size: 1 };
     }
-    Rounds.update({ _id: this._id }, toSet);
+    Rounds.update(this._id, toSet);
     template.$(".modal").modal('hide');
   },
 });
@@ -389,9 +389,9 @@ Template.modalHardCutoff.events({
     var $inputHardCutoff = template.$('[name="inputHardCutoff"]');
     var time = $inputHardCutoff.jChester('getSolveTime');
 
-    Rounds.update({
-      _id: this._id,
-    }, {
+    Rounds.update(
+      _this._id,
+    {
       $set: {
         // Explicitly listing all the fields in SolveTime as a workaround for
         //  https://github.com/aldeed/meteor-simple-schema/issues/202

both/access_control.js
@@ -6,7 +6,7 @@ getCannotManageCompetitionReason = function(userId, competitionId) {
   if(!user) {
     return new Meteor.Error(401, "User " + userId + " not found");
   }
-  var competition = Competitions.findOne({ _id: competitionId }, { fields: { _id: 1 } });
+  var competition = Competitions.findOne(competitionId, { fields: { _id: 1 } });
   if(!competition) {
     return new Meteor.Error(404, "Competition does not exist");
   }

both/devel_account.js
@@ -18,7 +18,7 @@ if(Meteor.isServer) {
           dob: new Date(),
         }
       });
-      var develUser = Meteor.users.findOne({ _id: develUserId });
+      var develUser = Meteor.users.findOne(develUserId);
       assert(develUser);
       // Mark email as verified, and user as a site admin
       Meteor.users.update({

both/methods.js
@@ -5,7 +5,7 @@ var throwIfNotVerifiedUser = function(userId) {
     throw new Meteor.Error(401, "Must log in");
   }
 
-  var user = Meteor.users.findOne({ _id: userId });
+  var user = Meteor.users.findOne(userId);
   if(!user.emails[0].verified) {
     throw new Meteor.Error(401, "Must verify email");
   }
@@ -16,7 +16,7 @@ var throwIfNotSiteAdmin = function(userId) {
     throw new Meteor.Error(401, "Must log in");
   }
 
-  var user = Meteor.users.findOne({ _id: userId });
+  var user = Meteor.users.findOne(userId);
   if(!user.siteAdmin) {
     throw new Meteor.Error(401, "Must be a site admin");
   }
@@ -71,7 +71,7 @@ Meteor.methods({
     }
     throwIfNotVerifiedUser(this.userId);
 
-    var user = Meteor.users.findOne({ _id: this.userId });
+    var user = Meteor.users.findOne(this.userId);
     if(!user.profile) {
       throw new Meteor.Error(400, "Must set up user profile");
     }
@@ -154,10 +154,10 @@ Meteor.methods({
       throw new Meteor.Error(400, "Cannot remove round.");
     }
 
-    var deadRound = Rounds.findOne({ _id: roundId });
+    var deadRound = Rounds.findOne(roundId);
     assert(deadRound);
 
-    Rounds.remove({ _id: roundId });
+    Rounds.remove(roundId);
     [RoundProgresses, Results, Groups, ScheduleEvents].forEach(function(collection) {
       collection.remove({ roundId: roundId });
     });
@@ -203,11 +203,11 @@ Meteor.methods({
   removeScheduleEvent: function(scheduleEventId) {
     var event = ScheduleEvents.findOne(scheduleEventId);
     throwIfCannotManageCompetition(this.userId, event.competitionId);
-    ScheduleEvents.remove({ _id: scheduleEventId});
+    ScheduleEvents.remove(scheduleEventId);
   },
   addOrUpdateGroup: function(newGroup) {
     throwIfCannotManageCompetition(this.userId, newGroup.competitionId);
-    var round = Rounds.findOne({ _id: newGroup.roundId });
+    var round = Rounds.findOne(newGroup.roundId);
     if(!round) {
       throw new Meteor.Error("Invalid roundId: '" + newGroup.roundId +"'");
     }
@@ -221,7 +221,7 @@ Meteor.methods({
     });
     if(existingGroup) {
       log.l0("Clobbering existing group", existingGroup);
-      Groups.update({ _id: existingGroup._id }, { $set: newGroup });
+      Groups.update(existingGroup._id, { $set: newGroup });
     } else {
       Groups.insert(newGroup);
     }
@@ -292,9 +292,9 @@ Meteor.methods({
     // or to update their check-in because the set of events they are registered for
     // changed. The latter may involve deleting results with data entered, so
     // be sure before you call this method =).
-    var registration = Registrations.findOne({
-      _id: registrationId,
-    }, {
+    var registration = Registrations.findOne(
+      registrationId,
+    {
       fields: {
         competitionId: 1,
         registeredEvents: 1,
@@ -338,14 +338,14 @@ Meteor.methods({
       // Update round progress
       RoundSorter.addRoundToSort(round._id);
     });
-    Registrations.update({ _id: registration._id }, { $set: { checkedInEvents: registration.registeredEvents } });
+    Registrations.update(registration._id, { $set: { checkedInEvents: registration.registeredEvents } });
   },
   addSiteAdmin: function(newSiteAdminUserId) {
     if(!isSiteAdmin(this.userId)) {
       throw new Meteor.Error(403, "Must be a site admin");
     }
 
-    Meteor.users.update({ _id: newSiteAdminUserId }, { $set: { siteAdmin: true } });
+    Meteor.users.update(newSiteAdminUserId, { $set: { siteAdmin: true } });
   },
   removeSiteAdmin: function(siteAdminToRemoveUserId) {
     if(!isSiteAdmin(this.userId)) {
@@ -357,7 +357,7 @@ Meteor.methods({
       throw new Meteor.Error(403, "Site admins may not unresign themselves!");
     }
 
-    Meteor.users.update({ _id: siteAdminToRemoveUserId }, { $set: { siteAdmin: false } });
+    Meteor.users.update(siteAdminToRemoveUserId, { $set: { siteAdmin: false } });
   },
   setSolveTime: setSolveTime,
   setRoundSoftCutoff: function(roundId, softCutoff) {
@@ -561,7 +561,7 @@ if(Meteor.isServer) {
         newCompetition.wcaCompetitionId = wcaCompetitionId;
       }
       var competitionId = Competitions.insert(newCompetition);
-      var competition = Competitions.findOne({ _id: competitionId });
+      var competition = Competitions.findOne(competitionId);
       assert(competition);
 
       var registrationByWcaJsonId = {};
@@ -593,7 +593,7 @@ if(Meteor.isServer) {
           registeredEvents: [],
           checkedInEvents: [],
         });
-        var registration = Registrations.findOne({ _id: registrationId });
+        var registration = Registrations.findOne(registrationId);
 
         assert(!registrationByWcaJsonId[wcaPerson.id]);
         registrationByWcaJsonId[wcaPerson.id] = registration;
@@ -706,7 +706,7 @@ if(Meteor.isServer) {
               softCutoff.time.moveCount -= 1;
             }
             log.l0("Setting soft cutoff for", wcaEvent.eventId, "round", nthRound + 1, "to", softCutoff);
-            Rounds.update({ _id: roundId }, { $set: { softCutoff: softCutoff } });
+            Rounds.update(roundId, { $set: { softCutoff: softCutoff } });
           }
 
           // We don't actually need to resort, but we do want to recompute
@@ -737,9 +737,9 @@ if(Meteor.isServer) {
       for(var jsonId in registrationByWcaJsonId) {
         if(registrationByWcaJsonId.hasOwnProperty(jsonId)) {
           var registration = registrationByWcaJsonId[jsonId];
-          var registrationId = Registrations.update({
-            _id: registration._id,
-          }, {
+          var registrationId = Registrations.update(
+            registration._id,
+          {
             $set: {
               registeredEvents: _.keys(registration.registeredEvents),
               checkedInEvents: _.keys(registration.checkedInEvents),
@@ -753,7 +753,7 @@ if(Meteor.isServer) {
     recomputeWhoAdvanced: function(roundId) {
       check(roundId, String);
 
-      var round = Rounds.findOne({ _id: roundId });
+      var round = Rounds.findOne(roundId);
       var nextRound = Rounds.findOne({
         competitionId: round.competitionId,
         eventCode: round.eventCode,
@@ -778,7 +778,7 @@ if(Meteor.isServer) {
           // advanced.
           advanced = false;
         }
-        Results.update({ _id: result._id }, { $set: { advanced: advanced } });
+        Results.update(result._id, { $set: { advanced: advanced } });
       });
     },
   });

both/routes.js
@@ -51,9 +51,9 @@ if(Meteor.isClient) {
     // current (new) page to get put in the history as the title of the
     // previous (old) page. Waiting for a little bit before setting the title
     // lets the browser history look sane.
-    setTimeout(function() {
+    Meteor.defer(function() {
       document.title = titleParts.reverse().join(' - ');
-    }, 0);
+    });
   });
 }
 

client/competitionRegistration.js
@@ -104,7 +104,7 @@ Template.competitionRegistration.helpers({
 
   registrationAskAboutGuests: function() {
     var competitionId = this.competitionId;
-    var competition = Competitions.findOne({ _id: competitionId }, { fields: { registrationAskAboutGuests: 1 } });
+    var competition = Competitions.findOne(competitionId, { fields: { registrationAskAboutGuests: 1 } });
     return competition.registrationAskAboutGuests;
   }
 });
@@ -123,7 +123,7 @@ Template.competitionRegistration.events({
     var competitionId = this.competitionId;
     var userId = Meteor.userId();
     var registration = getUserRegistration(userId, competitionId);
-    Registrations.remove({ _id: registration._id });
+    Registrations.remove(registration._id);
 
     $('#modalConfirmDeregistration').modal('hide');
   },

client/dataEntry.js
@@ -190,7 +190,7 @@ Template.roundDataEntry.helpers({
   },
   selectedSolves: function() {
     var selectedResultId = selectedResultIdReact.get();
-    var result = Results.findOne({ _id: selectedResultId }, { fields: { solves: 1 } });
+    var result = Results.findOne(selectedResultId, { fields: { solves: 1 } });
     var round = Rounds.findOne(this.roundId);
     var solves = result.solves || [];
     while(solves.length < round.format().count) {
@@ -244,13 +244,13 @@ function userResultMaybeSelected(template, roundId, jChesterToFocusIndex) {
   }
 
   selectedResultIdReact.set(result._id);
-  setTimeout(function() {
+  Meteor.defer(function() {
     var $jChesters = template.$('.jChester');
     if(jChesterToFocusIndex < 0) {
       jChesterToFocusIndex = $jChesters.length + jChesterToFocusIndex;
     }
     $jChesters.eq(jChesterToFocusIndex).focus();
-  }, 0);
+  });
 }
 
 function jChesterSave($jChester) {
@@ -287,10 +287,10 @@ Template.roundDataEntry.events({
     var $row = $(e.currentTarget);
     var resultId = $row.data('result-id');
 
-    var result = Results.findOne({ _id: resultId }, { fields: { registrationId: 1 } });
+    var result = Results.findOne(resultId, { fields: { registrationId: 1 } });
     assert(result);
 
-    var registration = Registrations.findOne({ _id: result.registrationId }, { fields: { uniqueName: 1 } });
+    var registration = Registrations.findOne(result.registrationId, { fields: { uniqueName: 1 } });
     assert(registration);
 
     var $inputParticipantName = template.$('#inputParticipantName');

client/editCompetition.js
@@ -11,7 +11,7 @@ setCompetitionAttribute = function(competitionId, attribute, value) {
     toSet[attribute] = value;
     update = { $set: toSet };
   }
-  Competitions.update({ _id: competitionId }, update);
+  Competitions.update(competitionId, update);
 };
 
 var setCompetitionLocationMap = function() {
@@ -130,12 +130,12 @@ function getExpandableListSettings(competitionId, registrationAttribute) {
     addDoc: function(registrationId) {
       var $toSet = {};
       $toSet[registrationAttribute] = true;
-      Registrations.update({ _id: registrationId }, { $set: $toSet });
+      Registrations.update(registrationId, { $set: $toSet });
     },
     removeDoc: function(registrationId) {
       var $toSet = {};
       $toSet[registrationAttribute] = false;
-      Registrations.update({ _id: registrationId }, { $set: $toSet });
+      Registrations.update(registrationId, { $set: $toSet });
     },
     docCriteria: criteria,
     isDeletable: function(registration) {
@@ -147,7 +147,7 @@ function getExpandableListSettings(competitionId, registrationAttribute) {
 Template.editCompetition.helpers({
   defaultCompetitionDataDocument: function() {
     var competitionId = this.competitionId;
-    var competition = Competitions.findOne({ _id: competitionId });
+    var competition = Competitions.findOne(competitionId);
 
     if(competition) {
       return competition;
@@ -202,9 +202,9 @@ Template.editCompetition.events({
     // up attributes of a competition that no longer exists.
     Router.go('home');
     var that = this;
-    setTimeout(function() {
+    Meteor.defer(function() {
       Meteor.call("deleteCompetition", that.competitionId);
-    }, 0);
+    });
   },
 });
 

commit 2ef8a12de46928ae46aa6ccb0a4c79f4553c41a1

_this -> this. This was accidentally broken in 9c067e58.

client/editEvents.js
@@ -373,9 +373,7 @@ Template.modalHardCutoff.events({
     let $inputHardCutoff = template.$('[name="inputHardCutoff"]');
     let time = $inputHardCutoff.jChester('getSolveTime');
 
-    Rounds.update(
-      _this._id,
-    {
+    Rounds.update(this._id, {
       $set: {
         // Explicitly listing all the fields in SolveTime as a workaround for
         //  https://github.com/aldeed/meteor-simple-schema/issues/202