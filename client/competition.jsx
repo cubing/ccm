56ea22caafca93cf6d0cb45fdd514d6e7d14f6e9
Caleb Hoover <choover11@gmail.com>
2016-05-11 14:19:40 -0700

    ported competition and competitionEvents to react

---
+++ client/competition.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const CompetitionView = React.createClass({
+  registeredForCompetition: function() {
+    let registration = Registrations.findOne({
+      competitionId: this.props.competitionId,
+      userId: this.props.userId,
+    }, {
+      fields: { _id: 1 }
+    });
+    return !!registration;
+  },
+
+  competitionIsScheduled: function() {
+    return Competitions.findOne(this.props.competitionId).startDate;
+  },
+
+  dateInterval: function() {
+    let comp = Competitions.findOne(this.props.competitionId);
+    return $.fullCalendar.formatRange(moment(comp.startDate).utc(), moment(comp.endDate()).utc(), "LL");
+  },
+
+  render () {
+    let {competitionId, competitionUrlId} = this.props;
+    let name = Competitions.findOne(this.props.competitionId).competitionName;
+    
+    return (
+      <div className='container'>
+        <h1>{name}</h1>
+        <p>
+          {this.competitionIsScheduled() ? 
+            <a href={`${competitionUrlId}/schedule`}>{this.dateInterval()}</a> : 'Unscheduled'
+          }
+        </p>
+        <p>
+          <a href={`${competitionUrlId}/registration`}>
+            {this.registeredForCompetition() ? 'Edit registration': 'Register here'}
+          </a>
+        </p>
+      </div>
+    );
+  }
+});
+
+Template.competition.rendered = function () {
+  let template = this;
+  console.log(template);
+  template.autorun(() => {
+    ReactDOM.render(
+      <CompetitionView 
+        competitionId={template.data.competitionId}
+        competitionUrlId={template.data.competitionUrlId}
+        userId={Meteor.userId()}/>,
+        template.$(".reactRenderArea")[0]
+    );
+  });
+}
