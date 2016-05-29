ff356e40a690d0139dbc8828ad910a588837dd77
Caleb Hoover <choover11@gmail.com>
2016-05-29 13:09:58 -0700

    added blaze to react component

---
+++ client/blazeToReact.jsx
@@ -0,0 +1,39 @@
+// TODO - this should really get pulled out into a Meteor (or npm?) package.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+BlazeToReact = React.createClass({
+  mixins: [ReactMeteorData],
+  renderBlaze() {
+    this.removeBlaze();
+    this.view = Blaze.renderWithData(Template[this.props.blazeTemplate], _.omit(this.props, 'blazeTemplate'), ReactDOM.findDOMNode(this.refs[this.props.blazeTemplate]));
+  },
+  removeBlaze() {
+    if (this.view) Blaze.remove(this.view);
+  },
+  getMeteorData() {
+    // Ensure a re-rendering of the template if a prop changes
+    
+    if (this._isMounted) {
+      this.renderBlaze();
+    }
+    return this.props;
+  },
+  componentDidUpdate() {
+    // Needed when used with a conditional show, like {condition ? <BlazeToCreact ../> : null}
+    this.renderBlaze();
+  },
+  componentDidMount() {
+    this._isMounted = true;
+    this.renderBlaze();
+  },
+  componentWillUnmount() {
+    this._isMounted = false;
+    this.removeBlaze();
+  },
+  render() {
+    return React.createElement(this.props.wrapperTag, { className: this.props.wrapperClass, ref: this.props.blazeTemplate });
+  }
+});
+
+export default BlazeToReact;
