7583352191776941e37d45387393c01f2892dd6f
Caleb Hoover <choover11@gmail.com>
2016-07-07 17:40:26 -0700

    basic editing of comp info

--- imports/ui/pages/manage/editCompetition.jsx
+++ imports/ui/pages/manage/editCompetition.jsx
@@ -4,20 +4,6 @@ import {createContainer} from 'meteor/react-meteor-data';
 
 const log = logging.handle("editCompetition");
 
-function setCompetitionAttribute(competitionId, attribute, value) {
-  let update;
-  if(value === null || typeof value === "undefined") {
-    let toUnset = {};
-    toUnset[attribute] = 1;
-    update = { $unset: toUnset };
-  } else {
-    let toSet = {};
-    toSet[attribute] = value;
-    update = { $set: toSet };
-  }
-  Competitions.update(competitionId, update);
-};
-
 const EditCompetition = React.createClass({
   getDefaultProps() {
     return {
@@ -28,10 +14,44 @@ const EditCompetition = React.createClass({
 
   getInitialState() {
     return {
+      competitionName: '',
+      lislted: false,
     };
   },
 
-  defaultCompetitionDataDocument: function() {
+  componentWillReceiveProps(props) {
+    if (props.competition) {
+      this.state.competitionName = props.competition.competitionName;
+      this.state.listed = props.competition.listed;
+    }
+  },
+
+  setCompetitionAttribute(attribute, value) {
+    let update;
+    if(value === null || typeof value === "undefined") {
+      update = {
+        $unset: {
+          [attribute]: 1
+        }
+      };
+    } else {
+      update = {
+        $set: {
+          [attribute]: value
+        }
+      };
+    }
+    Competitions.update(this.props.competitionId, update);
+  },
+
+  set(key, value) {
+   this.setCompetitionAttribute(key, value);
+    this.setState({
+      [key]: value
+    });
+  },
+
+  defaultCompetitionDataDocument() {
     let competitionId = this.props.competitionId;
     let competition = Competitions.findOne(competitionId);
 
@@ -44,48 +64,79 @@ const EditCompetition = React.createClass({
     }
   },
 
+  setListed(listed) {
+    this.set('listed', listed);
+  },
+
   render() {
-    const {ready, user, competition} = this.props;
-    let listed = false;
+    const {ready, user, competition, competitionId, competitionUrlId} = this.props;
+    let listed = competition ? competition.listed : this.state.listed;
 
     return (
       <div className='container'>
-        <div className="panel-heading">
-          <h3 className="panel-title">
-            Administrative settings
-          </h3>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">
+              Administrative settings
+            </h3>
+          </div>
+
+          { ready ?
+            <div className="panel-body">
+              <form className="form-horizontal" id="competitionAttributes" role="form">
+                <div className="form-group">
+                  <label htmlFor="inputWcaCompetitionId" className="col-sm-3 col-lg-2 control-label">WCA Competition ID</label>
+                  <div className="col-sm-9 col-lg-10">
+                    <input type="text" className="form-control" id="inputWcaCompetitionId" name="wcaCompetitionId" value={competition.wcaCompetitionId} disabled={user && user.siteAdmin}/>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-3 col-lg-2 control-label">Visibility</label>
+                  <div className="col-sm-9 col-lg-10">
+                    {user && user.siteAdmin ?
+                      <div id="toggleCompetitionListed" className="btn-group">
+                        <button type="button" className={`btn btn-default ${listed ? 'active' : ''}`} onClick={() => this.setListed(true)}>
+                          Listed
+                        </button>
+                        <button type="button" className={`btn btn-default ${listed ? '' : 'active'}`} onClick={() => this.setListed(false)}>
+                          Hidden
+                        </button>
+                      </div> :
+                      <p className="form-control-static">
+                        {listed ? 'Listed' : 'Hidden'}
+                      </p>
+                    }
+                  </div>
+                </div>
+              </form>
+            </div>
+            : null }
         </div>
 
-        { ready ?
-          <div className="panel-body">
-            <form className="form-horizontal" id="competitionAttributes" role="form">
-              <div className="form-group">
-                <label for="inputWcaCompetitionId" className="col-sm-3 col-lg-2 control-label">WCA Competition ID</label>
-                <div className="col-sm-9 col-lg-10">
-                  <input type="text" className="form-control" id="inputWcaCompetitionId" name="wcaCompetitionId" value={competition.wcaCompetitionId} disabled={user && user.isSiteAdmin}/>
-                </div>
-              </div>
-              <div className="form-group">
-                <label className="col-sm-3 col-lg-2 control-label">Visibility</label>
-                <div className="col-sm-9 col-lg-10">
-                  {user && user.isSiteAdmin ?
-                    <div id="toggleCompetitionListed" className="btn-group">
-                      <button type="button" className={`btn btn-default ${listed ? 'active' : ''}`}>
-                        Listed
-                      </button>
-                      <button type="button" className={`btn btn-default ${listed ? '' : 'active'}`}>
-                        Hidden
-                      </button>
-                    </div> :
-                    <p className="form-control-static">
-                      {listed ? 'Listed' : 'Hidden'}
-                    </p>
-                  }
-                </div>
-              </div>
-            </form>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">
+              General settings for {competition ? competition.competitionName : null}
+            </h3>
           </div>
-          : null }
+
+          <div className="panel-body">
+
+            <p>Competition Name:</p>
+            <input type="text" className="form-control" value={this.state.competitionName}
+            onChange={e => this.set('competitionName', e.target.value)}/>
+
+            <div className="row">
+              <div className="col-sm-12">
+                <label className="control-label">&nbsp;</label>
+                <p className="form-control-static">
+                  <a href={FlowRouter.path('editSchedule', {competitionUrlId})}>Adjust schedule</a>
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
     );
   },
