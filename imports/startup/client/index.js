200fdf7999da66588e05c6518c0afde99e253b9a
Caleb Hoover <choover11@gmail.com>
2017-02-20 16:58:34 -0800

    removed okgrow analytics to fix #330 but will be added back later on

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,10 +1,1 @@
 import './router';
-
-// https://github.com/okgrow/analytics complains when
-// Meteor.settings.public.analyticsSettings is unset. Velocity ensures that
-// we see the message twice, which just drives me crazy. Here we just set
-// Meteor.settings.public.analyticsSettings to an empty object to squelch this
-// message.
-Meteor.settings = Meteor.settings || {};
-Meteor.settings.public = Meteor.settings.public || {};
-Meteor.settings.public.analyticsSettings = {};
