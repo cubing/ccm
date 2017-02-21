8e4c0180c810cffc6731f749e707b9b26e284680
Caleb Hoover <choover11@gmail.com>
2017-02-21 10:18:11 -0800

    removed outdated files

--- client/competitionSchedule.js
+++
@@ -1,6 +0,0 @@
-Template.competitionSchedule.rendered = function() {
-  let template = this;
-  let $calendar = template.$('#calendar');
-  let editable = false;
-  setupCompetitionCalendar(template, $calendar, editable);
-};
--- imports/ui/pages/manage/manageCheckin.jsx
+++ imports/ui/pages/manage/manageCheckin.jsx
@@ -8,6 +8,29 @@ const log = logging.handle("manageCheckin");
 
 let selectedRegistrationReact = new ReactiveVar(null);
 
+let stickyTableById = {};
+let stickyTableId = 0;
+
+const makeTableSticky = function($table) {
+  stickyTableId++;
+
+  $table.stickyTableHeaders();
+
+  // React freaks out if it finds dom nodes that share a data-reactid.
+  // Since $.stickyTableHeaders copies the <thead> we generated with react,
+  // that copy has a data-reactid. Explicitly removing this attribute seems to
+  // be enough to make react happy.
+  let $floatingHead = $table.find("thead.tableFloatingHeader");
+  $floatingHead.removeAttr('data-reactid');
+};
+
+const makeTableNotSticky = function($table) {
+  let id = $table.data("stickyTableId");
+  let $storedTable = stickyTableById[id];
+  delete stickyTableById[id];
+  $table.stickyTableHeaders('destroy');
+};
+
 Template.editRegistrationForm.events({
   'click .js-delete-registration': function() {
     let confirmStr = `Are you sure you want to delete the registration for ${this.uniqueName}?`;
