ebaaa4c92784ce3ead6946c1b4da43cd5144d187
Caleb Hoover <choover11@gmail.com>
2017-02-22 15:59:01 -0800

    added administerSite page; fixed #345

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 import '/imports/vendor/jquery-3.0.0.min.js';
+import 'react-select/dist/react-select.css';
 import '/imports/vendor/bootstrap.min.js';
 
 // get rid of Maximum CallStack errors caused by bootbox and react-bootstrap modals
